082b393916861392feb5b7268c2a38b29bcd4408
Carlos Mougan <carmougan@gmail.com>
2021-10-12 10:27:16 +0200

    init

---
+++ Makefile
@@ -0,0 +1,36 @@
+black:
+	python -m black .
+	python -m black stupid.py
+
+gitall:
+	git add .
+	@read -p "Enter commit message: " message; 	git commit -m "$$message"
+	git push
+
+
+try:
+	@echo $$FOO
+
+stupid:
+	python -W ignore stupid.py
+
+test:
+	pytest testCN/testCN.py
+
+
+install_requirements:
+	pip install -r requirements.txt
+
+
+notebook_memory_usage:
+	conda install -c conda-forge jupyter-resource-usage
+	jupyter serverextension enable --py jupyter-resource-usage --sys-prefix
+	jupyter nbextension install --py jupyter-resource-usage --sys-prefix
+	jupyter nbextension enable --py jupyter-resource-usage --sys-prefix
+
+install_some_packages:
+	conda install pip
+	pip install jedi==0.17.2
+
+test:
+	python -m pytest tests
---
+++ tests/test_shift.py
@@ -0,0 +1,126 @@
+import unittest
+import pytest
+
+import numpy as np
+import pandas as pd
+
+from distributions import DistributionShift
+
+
+class TestDistributionShift(unittest.TestCase):
+    def setUp(self):
+        self.X = pd.DataFrame(
+            np.array([[1.0, 2.0, 3.0], [3.0, 4.0, 5.0]]), columns=["a", "b", "c"]
+        )
+        self.X_array = np.array([[1.0, 2.0, 3.0], [3.0, 4.0, 5.0]])
+        self.y = np.array([0, 1.0, 2.0])
+
+    def test_strategy_assert(self):
+        with pytest.raises(ValueError):
+            DistributionShift(strategy="aa")
+
+    def test_param_float(self):
+        with pytest.raises(ValueError):
+            DistributionShift(param="aa")
+
+    def test_covariateShift_1(self):
+        # Param = 1
+        db = DistributionShift(param=1.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([3, 4, 5]))
+
+    def test_covariateShift_2(self):
+        # Param = 2
+        db = DistributionShift(param=2.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([4, 5, 6]))
+
+    def test_covariateShift_10(self):
+        # Param = 10
+        db = DistributionShift(param=10.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X).mean(axis=0), np.array([12, 13, 14])
+        )
+
+    def test_covariateShift_onTransform(self):
+        # On transform
+        db = DistributionShift(param=1.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=1).mean(axis=0), np.array([3, 4, 5])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=2).mean(axis=0), np.array([4, 5, 6])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X, parameter=10).mean(axis=0), np.array([12, 13, 14])
+        )
+        # Param = 0
+        db = DistributionShift(param=0.0)
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([2, 3, 4]))
+
+    def test_handling_array(self):
+        # Param = 1
+        db = DistributionShift(param=1.0)
+        db.fit(self.X_array, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X_array).mean(axis=0), np.array([3, 4, 5])
+        )
+
+        # Param = 2
+        db = DistributionShift(param=2.0)
+        db.fit(self.X_array, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X_array).mean(axis=0), np.array([4, 5, 6])
+        )
+
+        # Param = 10
+        db = DistributionShift(param=10.0)
+        db.fit(self.X_array, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X_array).mean(axis=0), np.array([12, 13, 14])
+        )
+
+        # On transform
+        db = DistributionShift(param=1.0)
+        db.fit(self.X_array, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X_array, parameter=1).mean(axis=0), np.array([3, 4, 5])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X_array, parameter=2).mean(axis=0), np.array([4, 5, 6])
+        )
+        np.testing.assert_equal(
+            db.transform(self.X_array, parameter=10).mean(axis=0),
+            np.array([12, 13, 14]),
+        )
+
+        # Param = 0
+        db = DistributionShift(param=0.0)
+        db.fit(self.X_array, self.y)
+        np.testing.assert_equal(
+            db.transform(self.X_array).mean(axis=0), np.array([2, 3, 4])
+        )
+
+    def test_does_nothing(self):
+        db = DistributionShift()
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([2, 3, 4]))
+
+    def test_col_selection(self):
+        db = DistributionShift(cols=["a"], param=1)
+
+        db.fit(self.X, self.y)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([3, 3, 4]))
+
+    def test_col_selection_post(self):
+        db = DistributionShift(param=0)
+        db.fit(self.X, self.y)
+        # Check nothing happens
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([2, 3, 4]))
+
+        # Modify parameters
+        db.set_params(cols=["a"], param=1)
+        np.testing.assert_equal(db.transform(self.X).mean(axis=0), np.array([3, 3, 4]))
