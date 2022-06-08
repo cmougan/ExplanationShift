873024d529a296dc99c65a23cc839997b0d3d96e
Carlos Mougan <carmougan@gmail.com>
2022-06-08 08:53:31 +0200

    Performance quantification full script

--- folks/quantificationPerformance.py
+++ folks/quantificationPerformance.py
@@ -15,9 +15,11 @@ import pandas as pd
 from collections import defaultdict
 from scipy.stats import kstest, wasserstein_distance
 import seaborn as sns
+sns.set_style('whitegrid')
 import numpy as np
 import random
 import sys
+import matplotlib.pyplot as plt
 
 # Scikit-Learn
 from sklearn.pipeline import Pipeline
@@ -26,9 +28,16 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.linear_model import LogisticRegression, Lasso, LinearRegression
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import accuracy_score, roc_auc_score, mean_squared_error, mean_absolute_error
+from sklearn.metrics import (
+    accuracy_score,
+    roc_auc_score,
+    mean_squared_error,
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+)
 from sklearn.dummy import DummyRegressor
 from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
 
 # Specific packages
 from xgboost import XGBRegressor, XGBClassifier
@@ -41,15 +50,16 @@ import sys
 
 sys.path.append("../")
 from fairtools.utils import loop_estimators_fairness, psi, loop_estimators
+from ATC_opt import ATC
 
 # Seeding
 np.random.seed(0)
 random.seed(0)
-
+# %%
 # Load data
 data_source = ACSDataSource(survey_year="2014", horizon="1-Year", survey="person")
 ca_data = data_source.get_data(states=["CA"], download=True)
-data_source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
+data_source = ACSDataSource(survey_year="2016", horizon="1-Year", survey="person")
 mi_data = data_source.get_data(states=["HI"], download=True)
 # %%
 states = [
@@ -60,6 +70,9 @@ states = [
     "NE",
     "IL",
     "FL",
+]
+
+nooo = [
     "OK",
     "PA",
     "KS",
@@ -117,10 +130,10 @@ mi_features, mi_labels, mi_group = ACSEmployment.df_to_numpy(mi_data)
 ## Conver to DF
 ca_features = pd.DataFrame(ca_features, columns=ACSEmployment.features)
 mi_features = pd.DataFrame(mi_features, columns=ACSEmployment.features)
-
+# %%
 # Modeling
-model = XGBClassifier(verbosity=0, silent=True, use_label_encoder=False, njobs=1)
-
+# model = XGBClassifier(verbosity=0, silent=True, use_label_encoder=False, njobs=1)
+model = LogisticRegression()
 # Train on CA data
 preds_ca = cross_val_predict(
     model, ca_features, ca_labels, cv=3, method="predict_proba"
@@ -128,23 +141,25 @@ preds_ca = cross_val_predict(
 model.fit(ca_features, ca_labels)
 # Test on MI data
 preds_mi = model.predict_proba(mi_features)[:, 1]
-# %% 
-from ATC_opt import ATC
+# Threshold classifier
 atc = ATC()
-atc.fit(model.predict_proba(ca_features),ca_labels)
+atc.fit(model.predict_proba(ca_features), ca_labels)
 
 # %%
 ## Can we learn to solve this issue?
 ################################
 ####### PARAMETERS #############
-SAMPLE_FRAC = 5_00
-ITERS = 1_000
+SAMPLE_FRAC = 1_00
+ITERS = 20_0
 # Init
 train_error = accuracy_score(ca_labels, np.round(preds_ca))
 train_error_acc = accuracy_score(ca_labels, np.round(preds_ca))
 
 # xAI Train
-explainer = shap.Explainer(model)
+# explainer = shap.Explainer(model)
+explainer = shap.LinearExplainer(
+    model, ca_features, feature_dependence="correlation_dependent"
+)
 shap_test = explainer(ca_features)
 shap_test = pd.DataFrame(shap_test.values, columns=ca_features.columns)
 
@@ -152,15 +167,14 @@ shap_test = pd.DataFrame(shap_test.values, columns=ca_features.columns)
 mi_full = mi_features.copy()
 mi_full["group"] = mi_group
 mi_full["target"] = mi_labels
+
+
 def create_meta_data(test, samples, boots):
     # Init
     train = defaultdict()
     train_target_shift = defaultdict()
-    train_target_shift_ood = defaultdict()
-    train_ood = defaultdict()
     performance = defaultdict()
     train_shap = defaultdict()
-    train_shap_ood = defaultdict()
     atc_scores = defaultdict()
     for i in tqdm(range(0, boots), leave=False, desc="Test Bootstrap", position=1):
         # Initiate
@@ -169,7 +183,6 @@ def create_meta_data(test, samples, boots):
         row_shap = []
 
         # Sampling
-
         aux = test.sample(n=samples, replace=True)
 
         # Performance calculation
@@ -177,7 +190,11 @@ def create_meta_data(test, samples, boots):
         performance[i] = train_error - accuracy_score(aux.target, preds)
 
         # ATC
-        atc_scores[i] = atc.predict(model.predict_proba(aux.drop(columns=["target", "group"])))/100 - train_error_acc
+        atc_scores[i] = (
+            atc.predict(model.predict_proba(aux.drop(columns=["target", "group"])))
+            / 100
+            - train_error_acc
+        )
         # Shap values calculation
         shap_values = explainer(aux.drop(columns=["target", "group"]))
         shap_values = pd.DataFrame(shap_values.values, columns=ca_features.columns)
@@ -210,170 +227,95 @@ def create_meta_data(test, samples, boots):
 
     # On the target
     performance = pd.DataFrame(performance, index=[0]).T.values
-    return train_df,train_shap_df,train_target_shift_df,performance.squeeze(),atc_scores
+    return (
+        train_df,
+        train_shap_df,
+        train_target_shift_df,
+        performance.squeeze(),
+        atc_scores,
+    )
 
-input_tr,shap_tr,output_tr,model_error_tr,atc_scores= create_meta_data(mi_full, SAMPLE_FRAC, ITERS)
+
+input_tr, shap_tr, output_tr, model_error_tr, atc_scores = create_meta_data(
+    mi_full, SAMPLE_FRAC, ITERS
+)
 # %%
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-X_tr, X_te, y_tr, y_te = train_test_split(output_tr, model_error_tr, test_size=0.33, random_state=42)
-dummy = DummyRegressor().fit(X_tr, y_tr)
-print(mean_absolute_error(dummy.predict(X_te),y_te))
+estimators = defaultdict()
+X_tr, X_te, y_tr, y_te = train_test_split(
+    output_tr, model_error_tr, test_size=0.33, random_state=42
+)
+dummy = DummyRegressor(strategy="constant", constant=0).fit(X_tr, y_tr)
+print("Dummy", mean_absolute_error(dummy.predict(X_te), y_te))
 clf = LinearRegression()
-clf.fit(X_tr,y_tr)
-mean_absolute_error(clf.predict(X_te),y_te)
-# %%
-mean_absolute_error(pd.DataFrame(atc_scores.values())/100,model_error_tr)
+# clf = XGBRegressor()
+clf.fit(X_tr, y_tr)
+print("CLF", mean_absolute_error(clf.predict(X_te), y_te))
+# %%
+print("ATC", mean_absolute_error(pd.DataFrame(atc_scores.values()), model_error_tr))
 # %%
 sns.kdeplot(model_error_tr)
 # %%
-lksd
-# %%
-# Trainning set
-for i in tqdm(range(0, ITERS), leave=False, desc="Test Bootstrap", position=1):
-    # Initiate
-    row = []
-    row_target_shift = []
-    row_shap = []
-
-    # Sampling
-    aux = mi_full.sample(n=SAMPLE_FRAC, replace=True)
-
-    # Performance calculation
-    preds = model.predict_proba(aux.drop(columns=["target", "group"]))[:, 1]
-    performance[i] = train_error - roc_auc_score(aux.target, preds)
-
-    # Shap values calculation
-    shap_values = explainer(aux.drop(columns=["target", "group"]))
-    shap_values = pd.DataFrame(shap_values.values, columns=ca_features.columns)
-
-    for feat in ca_features.columns:
-        # Michigan
-        ks = psi(ca_features[feat], aux[feat])
-        sh = psi(shap_test[feat], shap_values[feat])
-
-        row.append(ks)
-        row_shap.append(sh)
-    # Target shift
-    ks_target_shift = wasserstein_distance(preds_ca, preds)
-    row_target_shift.append(ks_target_shift)
-    # Save test
-
-    train_shap[i] = row_shap
-    train[i] = row
-    train_target_shift[i] = row_target_shift
-
-# Save results
-## Train (previous test)
-train_df = pd.DataFrame(train).T
-train_df.columns = ca_features.columns
-
-train_shap_df = pd.DataFrame(train_shap).T
-train_shap_df.columns = ca_features.columns
-train_shap_df = train_shap_df.add_suffix("_shap")
-
-train_target_shift_df = pd.DataFrame(train_target_shift, index=[0]).T
-train_target_shift_df.columns = ["target"]
-
-# On the target
-performance = pd.DataFrame(performance, index=[0]).T.values
-## OOD State loop
-for state in tqdm(states, desc="States", position=0):
-    print(state)
-    # Initialize some lists
-    performance_ood = defaultdict()
-
+input_data = defaultdict()
+shap_data = defaultdict()
+output_data = defaultdict()
+model_error_data = defaultdict()
+atc_scores_data = defaultdict()
+for i, state in tqdm(enumerate(states), total=len(states)):
     # Load and process data
     tx_data = data_source.get_data(states=["HI"], download=True)
     tx_features, tx_labels, tx_group = ACSEmployment.df_to_numpy(tx_data)
     tx_features = pd.DataFrame(tx_features, columns=ACSEmployment.features)
-
-    # Lets add the target to ease the sampling
     tx_full = tx_features.copy()
     tx_full["group"] = tx_group
     tx_full["target"] = tx_labels
 
-    # Loop to create training data
-    for i in tqdm(range(0, int(ITERS)), leave=False, desc="Bootstrap", position=1):
-        row_ood = []
-        row_shap_ood = []
-        row_target_shift_ood = []
+    (
+        input_data[state],
+        shap_data[state],
+        output_data[state],
+        model_error_data[state],
+        atc_scores_data[state],
+    ) = create_meta_data(tx_full, SAMPLE_FRAC, int(ITERS/10))
+# %%
+plt.plot()
+for k in input_data.keys():
+    sns.kdeplot(model_error_data[k], label=k)
+plt.legend()
+plt.show()
+# %%
+# Evaluating stage
+## Input Data
+X_tr, X_te, y_tr, y_te = train_test_split(
+    input_tr, model_error_tr, test_size=0.33, random_state=42
+)
+dummy = DummyRegressor(strategy="constant", constant=0).fit(input_tr, model_error_tr)
+clf_input = LinearRegression().fit(input_tr, model_error_tr)
+clf_output = LinearRegression().fit(output_tr, model_error_tr)
+clf_shap = LinearRegression().fit(shap_tr, model_error_tr)
 
-        # Sampling
-        aux_ood = tx_full.sample(n=SAMPLE_FRAC, replace=True)
-
-        # OOD performance calculation
-        preds_ood = model.predict_proba(aux_ood.drop(columns=["target", "group"]))[:, 1]
-        performance_ood[i] = train_error - accuracy_score(
-            aux_ood.target.values, preds_ood
-        )
-
-        # Shap values calculation OOD
-        shap_values_ood = explainer(aux_ood.drop(columns=["target", "group"]))
-        shap_values_ood = pd.DataFrame(
-            shap_values_ood.values, columns=ca_features.columns
-        )
-
-        for feat in ca_features.columns:
-            # OOD
-            ks_ood = psi(ca_features[feat], aux_ood[feat])
-            sh_ood = psi(shap_test[feat], shap_values_ood[feat])
-            row_ood.append(ks_ood)
-            row_shap_ood.append(sh_ood)
-
-        # Target shift
-        ks_target_shift_ood = wasserstein_distance(preds_ca, preds_ood)
-        row_target_shift_ood = ks_target_shift_ood
-        # Save OOD
-        train_shap_ood[i] = row_shap_ood
-        train_ood[i] = row_ood
-        train_target_shift_ood[i] = row_target_shift_ood
-
-    # Save results
-    ## Test (previous OOD)
-    train_df_ood = pd.DataFrame(train_ood).T
-    train_df_ood.columns = ca_features.columns
-
-    train_shap_df_ood = pd.DataFrame(train_shap_ood).T
-    train_shap_df_ood.columns = ca_features.columns
-    train_shap_df_ood = train_shap_df_ood.add_suffix("_shap")
-
-    train_target_shift_df_ood = pd.DataFrame(train_target_shift_ood, index=[0]).T
-    train_target_shift_df_ood.columns = ["target"]
-
-    # Estimators for the loop
-    estimators = defaultdict()
-    estimators["Dummy"] = DummyRegressor()
-    estimators["Linear"] = Pipeline(
-        [("scaler", StandardScaler()), ("model", LinearRegression())]
-    )
-    estimators["RandomForest"] = RandomForestRegressor(random_state=0)
-    estimators["XGBoost"] = XGBRegressor(
-        verbosity=0, verbose=0, silent=True, random_state=0
-    )
-    estimators["SVM"] = Pipeline([("scaler", StandardScaler()), ("model", SVR(C=0.01))])
-    estimators["MLP"] = Pipeline(
-        [("scaler", StandardScaler()), ("model", MLPRegressor(random_state=0))]
-    )
-
-    ## Loop over different G estimators
-    # Some target transformations
-    performance_ood = pd.DataFrame(performance_ood, index=[0]).T.values
-
-    # Performance
-    loop_estimators_fairness(
-        estimator_set=estimators,
-        normal_data=train_df,
-        normal_data_ood=train_df_ood,
-        shap_data=train_shap_df,
-        shap_data_ood=train_shap_df_ood,
-        target_shift=train_target_shift_df,
-        target_shift_ood=train_target_shift_df_ood,
-        performance_ood=performance_ood,
-        target=performance,
-        state=state,
-        error_type="performance",
-    )
+input_results = []
+output_results = []
+shap_results = []
+dummy_results = []
+state_res = []
+atc_res = []
+for state in input_data.keys():
+    state_res.append(str(state))
+    input_results.append(mean_absolute_error(clf_input.predict(input_data[state]), model_error_data[state]))
+    output_results.append(mean_absolute_error(clf_output.predict(output_data[state]), model_error_data[state]))
+    shap_results.append(mean_absolute_error(clf_shap.predict(shap_data[state]), model_error_data[state]))
+    dummy_results.append(mean_absolute_error(dummy.predict(input_data[state]), model_error_data[state]))
+    atc_res.append(mean_absolute_error(pd.DataFrame(atc_scores_data[state].values()), model_error_data[state]))
 
 
+# %%
+res = pd.DataFrame([input_results, output_results, shap_results, dummy_results,atc_res,state_res]).T
+res = res.rename(columns={0: "input", 1: "output", 2: "shap", 3: "dummy",4:"atc",5:"state"})
+# %%
+aux = res.mean()
+sns.barplot(x=aux.index, y=aux.values)
+# %%
+plt.figure()
+aux.index
+plt.show()
 # %%
