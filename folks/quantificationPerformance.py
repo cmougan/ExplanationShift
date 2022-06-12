d4594ccd70fdad910e9df9a83292f38f6cee1506
Carlos Mougan <carmougan@gmail.com>
2022-06-12 18:56:40 +0200

    5 states loops

--- folks/quantificationPerformance.py
+++ folks/quantificationPerformance.py
@@ -25,7 +25,11 @@ import matplotlib.pyplot as plt
 # Scikit-Learn
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.ensemble import (
+    GradientBoostingClassifier,
+    RandomForestClassifier,
+    RandomForestRegressor,
+)
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.linear_model import LogisticRegression, Lasso, LinearRegression
 from sklearn.model_selection import cross_val_predict
@@ -37,7 +41,7 @@ from sklearn.metrics import (
     mean_absolute_percentage_error,
 )
 from sklearn.dummy import DummyRegressor
-from sklearn.svm import SVR
+from sklearn.svm import SVC, SVR
 from sklearn.model_selection import train_test_split
 
 # Specific packages
@@ -60,8 +64,9 @@ random.seed(0)
 # Load data
 data_source = ACSDataSource(survey_year="2014", horizon="1-Year", survey="person")
 ca_data = data_source.get_data(states=["CA"], download=True)
-data_source = ACSDataSource(survey_year="2016", horizon="1-Year", survey="person")
-mi_data = data_source.get_data(states=["HI"], download=True)
+ca_features, ca_labels, ca_group = ACSEmployment.df_to_numpy(ca_data)
+## Conver to DF
+ca_features = pd.DataFrame(ca_features, columns=ACSEmployment.features)
 # %%
 states = [
     "MI",
@@ -122,15 +127,7 @@ nooo = [
     "PR",
 ]
 
-data_source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
 
-# %%
-ca_features, ca_labels, ca_group = ACSEmployment.df_to_numpy(ca_data)
-mi_features, mi_labels, mi_group = ACSEmployment.df_to_numpy(mi_data)
-
-## Conver to DF
-ca_features = pd.DataFrame(ca_features, columns=ACSEmployment.features)
-mi_features = pd.DataFrame(mi_features, columns=ACSEmployment.features)
 # %%
 # Modeling
 # model = XGBClassifier(verbosity=0, silent=True, use_label_encoder=False, njobs=1)
@@ -140,8 +137,7 @@ preds_ca = cross_val_predict(
     model, ca_features, ca_labels, cv=3, method="predict_proba"
 )[:, 1]
 model.fit(ca_features, ca_labels)
-# Test on MI data
-preds_mi = model.predict_proba(mi_features)[:, 1]
+
 # Threshold classifier
 atc = ATC()
 atc.fit(model.predict_proba(ca_features), ca_labels)
@@ -150,8 +146,8 @@ atc.fit(model.predict_proba(ca_features), ca_labels)
 ## Can we learn to solve this issue?
 ################################
 ####### PARAMETERS #############
-SAMPLE_FRAC = 1_000
-ITERS = 5_00
+SAMPLE_FRAC = 100
+ITERS = 1_000
 # Init
 train_error = accuracy_score(ca_labels, np.round(preds_ca))
 train_error_acc = accuracy_score(ca_labels, np.round(preds_ca))
@@ -164,12 +160,8 @@ explainer = shap.LinearExplainer(
 shap_test = explainer(ca_features)
 shap_test = pd.DataFrame(shap_test.values, columns=ca_features.columns)
 
-# Lets add the target to ease the sampling
-mi_full = mi_features.copy()
-mi_full["group"] = mi_group
-mi_full["target"] = mi_labels
 
-
+## Meta data function
 def create_meta_data(test, samples, boots):
     # Init
     train = defaultdict()
@@ -237,118 +229,55 @@ def create_meta_data(test, samples, boots):
     )
 
 
-input_tr, shap_tr, output_tr, model_error_tr_, atc_scores = create_meta_data(
-    mi_full, SAMPLE_FRAC, ITERS
-)
 # %%
-# Convert in classification
-model_error_tr = np.where(model_error_tr_ < -0.02, 1, 0)
-sns.kdeplot(model_error_tr)
-# %%
-# Input
-X_tr, X_te, y_tr, y_te = train_test_split(
+res = defaultdict(list)
+# Loop throug each state
+for state in states:
+    print(state)
+    ## Lets add the target to ease the sampling
+    data_source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
+    mi_data = data_source.get_data(states=[state], download=True)
+    mi_features, mi_labels, mi_group = ACSEmployment.df_to_numpy(mi_data)
+    mi_features = pd.DataFrame(mi_features, columns=ACSEmployment.features)
+    mi_full = mi_features.copy()
+    mi_full["group"] = mi_group
+    mi_full["target"] = mi_labels
+
+    input_tr, shap_tr, output_tr, model_error_tr_, atc_scores = create_meta_data(
+        mi_full, SAMPLE_FRAC, ITERS
+    )
+    # Convert in classification
+    model_error_tr = np.where(model_error_tr_ < -0.05, 1, 0)
+    # Input
+    X_tr, X_te, y_tr, y_te = train_test_split(
         input_tr, model_error_tr, test_size=0.3, random_state=42
     )
-dummy = DummyRegressor(strategy="constant", constant=0).fit(X_tr, y_tr)
-print("Dummy", roc_auc_score(y_te, dummy.predict(X_te)))
-clf = LogisticRegression()
-
-clf.fit(X_tr, y_tr)
-print("Input", roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1]))
-
-# Shap
-X_tr, X_te, y_tr, y_te = train_test_split(
-        shap_tr, model_error_tr, test_size=0.3, random_state=42)
-clf = LogisticRegression()
-clf.fit(X_tr, y_tr)
-print("Shap", roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1]))
-
-# Output
-X_tr, X_te, y_tr, y_te = train_test_split(
+    dummy = DummyRegressor(strategy="constant", constant=0).fit(X_tr, y_tr)
+    print("Dummy", roc_auc_score(y_te, dummy.predict(X_te)))
+    clf = LogisticRegression()
+    clf.fit(X_tr, y_tr)
+    input_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
+    # Shap
+    X_tr, X_te, y_tr, y_te = train_test_split(
+        shap_tr, model_error_tr, test_size=0.3, random_state=42
+    )
+    clf.fit(X_tr, y_tr)
+    shap_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
+    # Output
+    X_tr, X_te, y_tr, y_te = train_test_split(
         output_tr, model_error_tr, test_size=0.3, random_state=42
     )
-clf = LogisticRegression()
-clf.fit(X_tr, y_tr)
-print("Output", roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1]))
-# ATC
-print("ATC", roc_auc_score(model_error_tr,np.where(pd.DataFrame(atc_scores.values(),columns=['values']).values<-0.02,1,0)))
-
-# %%
-estimators = defaultdict()
-input_data = defaultdict()
-shap_data = defaultdict()
-output_data = defaultdict()
-model_error_data = defaultdict()
-atc_scores_data = defaultdict()
-for i, state in tqdm(enumerate(states), total=len(states)):
-    # Load and process data
-    tx_data = data_source.get_data(states=["HI"], download=True)
-    tx_features, tx_labels, tx_group = ACSEmployment.df_to_numpy(tx_data)
-    tx_features = pd.DataFrame(tx_features, columns=ACSEmployment.features)
-    tx_full = tx_features.copy()
-    tx_full["group"] = tx_group
-    tx_full["target"] = tx_labels
-
-    (
-        input_data[state],
-        shap_data[state],
-        output_data[state],
-        model_error_data[state],
-        atc_scores_data[state],
-    ) = create_meta_data(tx_full, SAMPLE_FRAC, int(ITERS / 10))
-# %%
-plt.plot()
-for k in input_data.keys():
-    sns.kdeplot(model_error_data[k], label=k)
-plt.legend()
-plt.show()
-# Evaluating stage
-## Input Data
-X_tr, X_te, y_tr, y_te = train_test_split(
-    input_tr, model_error_tr, test_size=0.33, random_state=42
-)
-dummy = DummyRegressor(strategy="constant", constant=0).fit(input_tr, model_error_tr)
-
-clf_input = XGBClassifier().fit(input_tr, model_error_tr)
-clf_output = XGBClassifier().fit(output_tr, model_error_tr)
-clf_shap = XGBClassifier().fit(shap_tr, model_error_tr)
-# %%
-input_results = []
-output_results = []
-shap_results = []
-dummy_results = []
-state_res = []
-atc_res = []
-for state in input_data.keys():
-    state_res.append(str(state))
-    input_results.append(
-        roc_auc_score(np.where(model_error_data[state]<-0.02,1,0),clf_input.predict(input_data[state]))
+    clf.fit(X_tr, y_tr)
+    output_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
+    # ATC
+    atc_results = roc_auc_score(
+        model_error_tr,
+        np.where(
+            pd.DataFrame(atc_scores.values(), columns=["values"]).values < -0.02,
+            0,
+            1,
+        ),
     )
-    output_results.append(
-        roc_auc_score(np.where(model_error_data[state]<-0.02,1,0), clf_output.predict(output_data[state]))
-    )
-    shap_results.append(
-        roc_auc_score(np.where(model_error_data[state]<-0.02,1,0), clf_shap.predict(shap_data[state]))
-    )
-    dummy_results.append(
-        roc_auc_score(np.where(model_error_data[state]<-0.02,1,0), dummy.predict(input_data[state]))
-    )
-    atc_res.append(roc_auc_score(np.where(model_error_data[state]<-0.02,1,0),np.where(pd.DataFrame(atc_scores_data[state].values(),columns=['values']).values<-0.02,1,0)))
-    #atc_res.append(roc_auc_score(pd.DataFrame(np.where(model_error_data[state]<-0.02,1,0), atc_scores_data[state].values())))
-
-
+    res[state] = [input_results, shap_results, output_results, atc_results]
 # %%
-res = pd.DataFrame(
-    [input_results, output_results, shap_results, dummy_results, atc_res, state_res]
-).T
-res = res.rename(
-    columns={0: "input", 1: "output", 2: "shap", 3: "dummy", 4: "atc", 5: "state"}
-)
-# %%
-aux = res.mean()
-sns.barplot(x=aux.index, y=aux.values)
-# %%
-plt.figure()
-aux.index
-plt.show()
-# %%
+pd.DataFrame(res)
