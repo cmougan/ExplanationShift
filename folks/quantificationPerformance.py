de1bac4bac94ca950bc6323cd37b63d5f9bf0366
Carlos Mougan <carmougan@gmail.com>
2022-07-30 16:30:57 -0500

    black

--- folks/quantificationPerformance.py
+++ folks/quantificationPerformance.py
@@ -255,20 +255,20 @@ for state in tqdm(states):
 
         # Input
         X_tr, X_te, y_tr, y_te = train_test_split(
-            input_tr, model_error_tr, test_size=0.3, random_state=42
+            input_tr, model_error_tr, test_size=0.9, random_state=42
         )
         clf = LogisticRegression()
         clf.fit(X_tr, y_tr)
         input_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
         # Shap
         X_tr, X_te, y_tr, y_te = train_test_split(
-            shap_tr, model_error_tr, test_size=0.3, random_state=42
+            shap_tr, model_error_tr, test_size=0.9, random_state=42
         )
         clf.fit(X_tr, y_tr)
         shap_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
         # Output
         X_tr, X_te, y_tr, y_te = train_test_split(
-            output_tr, model_error_tr, test_size=0.3, random_state=42
+            output_tr, model_error_tr, test_size=0.9, random_state=42
         )
         clf.fit(X_tr, y_tr)
         output_results = roc_auc_score(y_te, clf.predict_proba(X_te)[:, 1])
