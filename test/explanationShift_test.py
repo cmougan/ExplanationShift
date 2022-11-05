7eabc91e1364183360f82b3ca9fdedacccd676de
Carlos Mougan <carmougan@gmail.com>
2022-11-05 11:08:26 +0100

    this test not work

--- test/explanationShift_test.py
+++ test/explanationShift_test.py
@@ -7,7 +7,6 @@ from sklearn.datasets import make_blobs
 import pandas as pd
 
 
-
 X, y = make_blobs(n_samples=100, centers=2, n_features=5, random_state=0)
 X = pd.DataFrame(X, columns=["a", "b", "c", "d", "e"])
 
@@ -22,7 +21,8 @@ def test_return_shapDF():
     )
     esd.fit_model(X, y)
     ex = esd.get_explanations(X)
-    assert all([a == b for a, b in zip(ex.columns,X.columns)])
+    assert all([a == b for a, b in zip(ex.columns, X.columns)])
+
 
 def check_supported_models():
     # dont think this works
@@ -32,9 +32,13 @@ def check_supported_models():
 
     from sklearn.linear_model import LogisticRegression
     from xgboost import XGBClassifier, XGBRegressor
+
     for model in [XGBRegressor(), LogisticRegression()]:
         for gmodel in [XGBClassifier(), LogisticRegression()]:
-                assert type(ExplanationShiftDetector(model=model, gmodel=gmodel)) is  tools.xaiUtils.ExplanationShiftDetector
+            assert (
+                type(ExplanationShiftDetector(model=model, gmodel=gmodel))
+                is tools.xaiUtils.ExplanationShiftDetector
+            )
 
 
 def check_not_supported_models():
@@ -44,7 +48,7 @@ def check_not_supported_models():
     """
 
     from sklearn.linear_model import LogisticRegression
-    from sklearn.neural_network import MLPClassifier,MLPRegressor
+    from sklearn.neural_network import MLPClassifier, MLPRegressor
 
     with pytest.raises(ValueError):
         ExplanationShiftDetector(model=jjj, gmodel=LogisticRegression())
