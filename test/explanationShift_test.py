5dfba46a0bf96499aa0ecc616b2d5995328762f2
Carlos Mougan <carmougan@gmail.com>
2022-11-05 15:21:19 +0100

    clean docs + dataframe expcetion error

--- test/explanationShift_test.py
+++ test/explanationShift_test.py
@@ -9,21 +9,23 @@ import pandas as pd
 import numpy as np
 
 
-X, y = make_blobs(n_samples=100, centers=2, n_features=5, random_state=0)
-X = pd.DataFrame(X, columns=["a", "b", "c", "d", "e"])
+X, y = make_blobs(n_samples=2000, centers=2, n_features=5, random_state=0)
+X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.5, random_state=0)
+X_ood, y_ood = make_blobs(n_samples=1000, centers=1, n_features=5, random_state=0)
 
 
 def test_return_shapDF():
     """
     If X is dataframe, return shap values as dataframe.
     """
+    XX = pd.DataFrame(X, columns=["a", "b", "c", "d", "e"])
 
     esd = ExplanationShiftDetector(
         model=LinearRegression(), gmodel=LogisticRegression()
     )
-    esd.fit_model(X, y)
-    ex = esd.get_explanations(X)
-    assert all([a == b for a, b in zip(ex.columns, X.columns)])
+    esd.fit_model(XX, y)
+    ex = esd.get_explanations(XX)
+    assert all([a == b for a, b in zip(ex.columns, XX.columns)])
 
 
 def test_supported_models():
@@ -56,20 +58,15 @@ def test_doc_examples():
     """
     Check that doc examples work.
     """
-    from sklearn.metrics import roc_auc_score
-
-    XX, yy = make_blobs(n_samples=2000, centers=2, n_features=5, random_state=0)
-    XX_tr, XX_te, yy_tr, yy_te = train_test_split(XX, yy, test_size=0.5, random_state=0)
-    XX_ood, _ = make_blobs(n_samples=1000, centers=1, n_features=5, random_state=0)
 
     detector = ExplanationShiftDetector(
         model=XGBRegressor(random_state=0), gmodel=LogisticRegression()
     )
     # On OOD
-    detector.fit(X_source=XX_tr, y_source=yy_tr, X_ood=XX_ood)
+    detector.fit(X_source=X_tr, y_source=y_tr, X_ood=X_ood)
     assert np.round(detector.get_auc_val(), decimals=2) == 0.77
     # On test
-    detector.fit(X_source=XX_tr, y_source=yy_tr, X_ood=XX_te)
+    detector.fit(X_source=X_tr, y_source=y_tr, X_ood=X_te)
     assert np.round(detector.get_auc_val(), decimals=2) == 0.5
 
 
@@ -92,9 +89,9 @@ def test_get_coefs():
     esd = ExplanationShiftDetector(
         model=LinearRegression(), gmodel=LogisticRegression()
     )
-    esd.fit_model(X, y)
+    esd.fit(X, y, X_ood)
     coefs = esd.get_coefs()
     # Assert shape
-    assert coefs.shape == (X.shape[1],)
+    assert coefs.shape[1] == X.shape[1]
     # Assert that there is non NaNs
     assert not np.any(np.isnan(coefs))
