731c4ec343c9c07e09ffaf9105bc9945bb638e5a
Carlos Mougan <carmougan@gmail.com>
2022-12-23 13:53:16 +0100

    loop over states

--- quantification.py
+++ quantification.py
@@ -22,54 +22,69 @@ import seaborn as sns
 plt.style.use("seaborn-whitegrid")
 from xgboost import XGBClassifier
 
-
 from tools.datasets import GetData
 from tools.xaiUtils import ExplanationShiftDetector
 
 # %%
 data = GetData(type="real")
-X, y, X_ood, y_ood = data.get_data()
+X, y, _, _ = data.get_data()
 # Hold out set for CA-14
 X_cal_1, X_cal_2, y_cal_1, y_cal_2 = train_test_split(
     X, y, test_size=0.5, stratify=y, random_state=0
 )
 X, y = X_cal_1, y_cal_1
 # %%
-detector = ExplanationShiftDetector(
-    model=XGBClassifier(),
-    gmodel=Pipeline(
-        [
-            ("scaler", StandardScaler()),
-            ("lr", LogisticRegression(penalty="l1", solver="liblinear")),
-        ]
-    ),
-)
-# %%
-states = ["NY18", "TX18", "HI18", "MI18", "PR18", "CA18"]
+res = []
+states = ["NY18", "TX18", "MI18", "MN18", "WI18", "FL18"]
 for state in tqdm(states):
     X_ood, y_ood = data.get_state(state=state[:2], year="20" + state[2:])
     X_ood, X_ood_te, y_ood, y_ood_te = train_test_split(
         X_ood, y_ood, test_size=0.5, stratify=y_ood, random_state=0
     )
-    break
-# %% Build detector
-detector.fit(X, y, X_ood)
-# Performance of model on X_train hold out
-auc_tr = roc_auc_score(y_cal_2, detector.model.predict_proba(X_cal_2)[:, 1])
-# %%
-# Performance of detector on X_ood hold out
-preds = detector.predict_proba(X_ood_te)
-# %%
-X_ood_te["pred"] = preds[:, 1]
-X_ood_te["y"] = y_ood_te
-# %%
-X_ood_te = X_ood_te.sort_values("pred", ascending=True)
 
-N = 1000
-auc_ood = roc_auc_score(
-    X_ood_te.head(N).y,
-    detector.model.predict_proba(X_ood_te.head(N).drop(columns=["y", "pred"]))[:, 1],
-)
+    # Build detector
+    for space in ["explanation", "input", "prediction"]:
+        detector = ExplanationShiftDetector(
+            model=XGBClassifier(),
+            gmodel=Pipeline(
+                [
+                    ("scaler", StandardScaler()),
+                    ("lr", LogisticRegression(penalty="l1", solver="liblinear")),
+                ]
+            ),
+            space=space,
+        )
+        if "label" in X_ood.columns:
+            X_ood = X_ood.drop(columns=["label"])
+        detector.fit(X, y, X_ood)
+
+        # Performance of model on X_train hold out
+        auc_tr = roc_auc_score(y_cal_2, detector.model.predict_proba(X_cal_2)[:, 1])
+
+        # Performance of detector on X_ood hold out
+        preds = detector.predict_proba(X_ood_te)
+
+        # Exp Space
+        X_ood_te_ = X_ood_te.copy()
+        X_ood_te_["pred"] = preds[:, 1]
+        X_ood_te_["y"] = y_ood_te
+        X_ood_te_ = X_ood_te_.sort_values("pred", ascending=False)
+        N = 10000
+        auc_ood = roc_auc_score(
+            X_ood_te_.head(N).y,
+            detector.model.predict_proba(X_ood_te_.head(N).drop(columns=["y", "pred"]))[
+                :, 1
+            ],
+        )
+        res.append([space, state, auc_tr - auc_ood])
+
+results = pd.DataFrame(res, columns=["space", "state", "auc_diff"])
 # %%
-auc_tr - auc_ood
+# Convert results to table with State vs Space
+results = results.pivot(index="state", columns="space", values="auc_diff")
+# Highlight the column with the max value
+results.style.apply(
+    lambda x: ["background-color: lightgreen" if v == x.max() else "" for v in x],
+    axis=1,
+)
 # %%
