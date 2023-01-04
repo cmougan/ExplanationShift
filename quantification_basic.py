f4369fd2d7c1dffd600df6d6b7c26bfe92b5949f
Carlos Mougan <carmougan@gmail.com>
2023-01-04 23:11:06 +0100

    experiment with big gap: FAIL

--- quantification_basic.py
+++ quantification_basic.py
@@ -25,105 +25,85 @@ from tools.datasets import GetData
 from tools.explanationShift import ExplanationShiftDetector
 
 # %%
-data = GetData(type="real", datasets="ACSTravelTime")
+data = GetData(type="real", datasets="ACSIncome")
 X, y = data.get_state(state="CA", year="2014")
-# Split data into train, val and test
-X_tr, X_te, y_tr, y_te = train_test_split(
-    X, y, test_size=0.3, stratify=y, random_state=0
-)
+
+# What is the most important feature?
+model = XGBClassifier()
+model.fit(X, y)
+importances = model.feature_importances_
+# %%
+# We select the most important feature
+most_important = np.argmin(importances)
+# We sort the data by the most important feature
+X["label"] = y
+X = X.sort_values(by=X.columns[most_important], ascending=True)
+# Split data into first and second half
+X_1 = X.iloc[: int(len(X) / 3), :]
+X_2 = X.iloc[2 * int(len(X) / 3) :, :]
+y_1 = X_1["label"]
+y_2 = X_2["label"]
+X_1 = X_1.drop(columns=["label"])
+X_2 = X_2.drop(columns=["label"])
+# Split X_1 into train and val
 X_tr, X_val, y_tr, y_val = train_test_split(
-    X_tr, y_tr, test_size=0.5, stratify=y_tr, random_state=0
+    X_1, y_1, test_size=0.5, stratify=y_1, random_state=0
 )
-
-# OOD Data
-X_ood, y_ood = data.get_state(state="HI", year="2018")
+# Split X_2 into ood_tr and ood_te
 X_ood_tr, X_ood_te, y_ood_tr, y_ood_te = train_test_split(
-    X_ood, y_ood, test_size=0.5, stratify=y_ood, random_state=0
+    X_2, y_2, test_size=0.5, stratify=y_2, random_state=0
 )
-# Train data for G
-X_val["OOD"] = 0
-X_ood_tr["OOD"] = 1
-X_hold = pd.concat([X_val, X_ood_tr])
-z_hold_tr = X_hold["OOD"]
+# Concatenate X_te and X_ood_te
+X_val["ood"] = 0
+X_ood_te["ood"] = 1
+X_hold = pd.concat([X_val, X_ood_te])
+y_hold = pd.concat([y_val, y_ood_te])
+z_hold = X_hold["ood"]
+X_hold = X_hold.drop(columns=["ood"])
+X_val = X_val.drop(columns=["ood"])
+X_ood_te = X_ood_te.drop(columns=["ood"])
 
-# Drop columns to avoid errors
-X_hold = X_hold.drop(columns=["OOD"])
-X_val = X_val.drop(columns=["OOD"])
-X_ood_tr = X_ood_tr.drop(columns=["OOD"])
-# The real y
-y_hold_tr = pd.concat(
-    [
-        pd.DataFrame(y_val, columns=["real"]),
-        pd.DataFrame(y_ood_tr, columns=["real"]),
-    ]
-)
 # %%
-# Test data for G
-X_te["OOD"] = 0
-X_ood_te["OOD"] = 1
-X_hold_test = pd.concat([X_te, X_ood_te])
-z_hold_test = X_hold_test["OOD"]
-
-# Drop columns to avoid errors
-X_hold_test = X_hold_test.drop(columns=["OOD"])
-X_te = X_te.drop(columns=["OOD"])
-X_ood_te = X_ood_te.drop(columns=["OOD"])
-# The real y
-y_hold_test = pd.concat(
-    [
-        pd.DataFrame(y_te, columns=["real"]),
-        pd.DataFrame(y_ood_te, columns=["real"]),
-    ]
-)
-# %%
-for space in ["explanation", "input", "prediction"]:
+for space in ["input", "prediction", "explanation"]:
     print("----------------------------------")
     print(space)
     print("----------------------------------")
-    # Fit our estimator
     detector = ExplanationShiftDetector(
-        model=XGBClassifier(max_depth=10, random_state=0),
+        model=XGBClassifier(max_depth=3, random_state=0),
         gmodel=XGBClassifier(max_depth=3, random_state=0),
         space=space,
-        masker=False,
     )
     if "label" in X_ood_tr.columns:
         X_ood_tr = X_ood_tr.drop(columns=["label"])
 
     detector.fit(X_tr, y_tr, X_ood_tr)
-    # detector.explain_detector()
-
-    # Does the model decay?
-    print("Does the model decay?")
-    auc_te = np.round(roc_auc_score(y_te, detector.model.predict_proba(X_te)[:, 1]), 3)
-    auc_ood = np.round(
-        roc_auc_score(y_ood_te, detector.model.predict_proba(X_ood_te)[:, 1]), 3
+    # Evaluate the model
+    auc_tr = roc_auc_score(y_val, detector.model.predict_proba(X_val)[:, 1])
+    print(
+        "AUC on test set",
+        auc_tr,
     )
-    print("AUC TE", auc_te)
-    print("AUC OOD", auc_ood)
+    print(
+        "AUC on OOD test set",
+        roc_auc_score(y_ood_te, detector.model.predict_proba(X_ood_te)[:, 1]),
+    )
+    print("Auditor", detector.get_auc_val())
 
-    # Does the model G have good performance?
-    print("Does the model G have good performance?")
-    print("g: ", detector.get_auc_val())
-
-    # Analyze
-    aux = X_hold_test.copy()
-    aux["real"] = y_hold_test["real"].values
-    aux["pred"] = detector.model.predict(X_hold_test)
-    aux["pred_proba"] = detector.model.predict_proba(X_hold_test)[:, 1]
-    aux["ood"] = z_hold_test.values
-    aux["ood_pred"] = detector.predict(X_hold_test)
-    aux["ood_pred_proba"] = detector.predict_proba(X_hold_test)[:, 1]
+    aux = X_ood_te.copy()
+    aux["real"] = y_ood_te.values
+    aux["pred"] = detector.model.predict(X_ood_te)
+    aux["pred_proba"] = detector.model.predict_proba(X_ood_te)[:, 1]
+    # aux["ood"] = z_hold_test.values
+    aux["ood_pred"] = detector.predict(X_ood_te)
+    # aux["ood_pred_proba"] = detector.predict_proba(X_hold_test)[:, 1]
     print("Total flagged as OOD: ", aux[aux["ood_pred"] == 1].shape[0])
+    auc_ood = roc_auc_score(aux.real, aux.pred_proba.values)
 
     try:
         decay = roc_auc_score(
-            aux[aux["ood_pred"] == 0].real, aux[aux["ood_pred"] == 0].pred_proba.values
-        ) - roc_auc_score(
             aux[aux["ood_pred"] == 1].real, aux[aux["ood_pred"] == 1].pred_proba.values
         )
     except:
         decay = 0
-    print(space, decay)
-
-    # %%
+    print("AUC OOD", auc_ood)
+    print("Decay OOD", decay)
