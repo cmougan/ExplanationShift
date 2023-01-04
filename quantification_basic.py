4a3325fbdb05b92144246620225551a5c1fd1a23
Carlos Mougan <carmougan@gmail.com>
2023-01-04 17:28:48 +0100

    exp works better with xgb10

--- quantification_basic.py
+++ quantification_basic.py
@@ -13,6 +13,7 @@ random.seed(0)
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
 from tools.explanationShift import ExplanationShiftDetector
@@ -24,87 +25,105 @@ from tools.datasets import GetData
 from tools.explanationShift import ExplanationShiftDetector
 
 # %%
-data = GetData(type="real", datasets="ACSIncome")
+data = GetData(type="real", datasets="ACSTravelTime")
 X, y = data.get_state(state="CA", year="2014")
-# Hold out set for CA-14
-X_cal_1, X_cal_2, y_cal_1, y_cal_2 = train_test_split(
-    X, y, test_size=0.5, stratify=y, random_state=0
+# Split data into train, val and test
+X_tr, X_te, y_tr, y_te = train_test_split(
+    X, y, test_size=0.3, stratify=y, random_state=0
 )
-X, y = X_cal_1, y_cal_1
+X_tr, X_val, y_tr, y_val = train_test_split(
+    X_tr, y_tr, test_size=0.5, stratify=y_tr, random_state=0
+)
+
 # OOD Data
-X_ood, y_ood = data.get_state(state="NY", year="2018")
-X_ood, X_ood_te, y_ood, y_ood_te = train_test_split(
+X_ood, y_ood = data.get_state(state="HI", year="2018")
+X_ood_tr, X_ood_te, y_ood_tr, y_ood_te = train_test_split(
     X_ood, y_ood, test_size=0.5, stratify=y_ood, random_state=0
 )
-# The y to evaluate OOD detection
-X_cal_2["OOD"] = 0
-X_ood_te["OOD"] = 1
-X_hold = pd.concat([X_cal_2, X_ood_te])
-y_hold_ood = X_hold["OOD"]
+# Train data for G
+X_val["OOD"] = 0
+X_ood_tr["OOD"] = 1
+X_hold = pd.concat([X_val, X_ood_tr])
+z_hold_tr = X_hold["OOD"]
 
 # Drop columns to avoid errors
 X_hold = X_hold.drop(columns=["OOD"])
-X_cal_2 = X_cal_2.drop(columns=["OOD"])
+X_val = X_val.drop(columns=["OOD"])
+X_ood_tr = X_ood_tr.drop(columns=["OOD"])
+# The real y
+y_hold_tr = pd.concat(
+    [
+        pd.DataFrame(y_val, columns=["real"]),
+        pd.DataFrame(y_ood_tr, columns=["real"]),
+    ]
+)
+# %%
+# Test data for G
+X_te["OOD"] = 0
+X_ood_te["OOD"] = 1
+X_hold_test = pd.concat([X_te, X_ood_te])
+z_hold_test = X_hold_test["OOD"]
+
+# Drop columns to avoid errors
+X_hold_test = X_hold_test.drop(columns=["OOD"])
+X_te = X_te.drop(columns=["OOD"])
 X_ood_te = X_ood_te.drop(columns=["OOD"])
 # The real y
-y_hold = pd.concat(
+y_hold_test = pd.concat(
     [
-        pd.DataFrame(y_cal_2, columns=["real"]),
+        pd.DataFrame(y_te, columns=["real"]),
         pd.DataFrame(y_ood_te, columns=["real"]),
     ]
 )
 # %%
-# Fit our estimator
-detector = ExplanationShiftDetector(
-    model=XGBClassifier(max_depth=3, random_state=0),
-    gmodel=Pipeline(
-        [
-            ("scaler", StandardScaler()),
-            ("lr", LogisticRegression(penalty="l1", solver="liblinear")),
-        ]
-    ),
-    # model = LogisticRegression(),
-    space="input",
-    masker=True,
-)
+for space in ["explanation", "input", "prediction"]:
+    print("----------------------------------")
+    print(space)
+    print("----------------------------------")
+    # Fit our estimator
+    detector = ExplanationShiftDetector(
+        model=XGBClassifier(max_depth=10, random_state=0),
+        gmodel=XGBClassifier(max_depth=3, random_state=0),
+        space=space,
+        masker=False,
+    )
+    if "label" in X_ood_tr.columns:
+        X_ood_tr = X_ood_tr.drop(columns=["label"])
 
-detector.fit(X, y, X_ood)
-# %%
-# Evaluate on hold out
-print(
-    "f: ", roc_auc_score(y_hold.real.values, detector.model.predict_proba(X_hold)[:, 1])
-)
-print("g: ", detector.get_auc_val())
-print("g2: ", roc_auc_score(y_hold_ood, detector.predict_proba(X_hold)[:, 1]))
+    detector.fit(X_tr, y_tr, X_ood_tr)
+    # detector.explain_detector()
 
-# %%
-aux = X_hold.copy()
-aux["OOD"] = detector.predict_proba(X_hold)[:, 1]
-aux["error"] = np.abs(y_hold.real.values - detector.model.predict_proba(X_hold)[:, 1])
-# %%
-aux = aux.sort_values(by="error", ascending=False).reset_index(drop=True)
-# %%
-plt.figure(figsize=(10, 10))
-plt.scatter(aux.index, aux["error"], label="Error")
-plt.scatter(aux.index, aux["OOD"], label="OOD Explanations")
-plt.xlabel("Index")
-plt.ylabel("Error")
-plt.title("Error vs Index")
-plt.legend()
-plt.show()
-# %%
-plt.figure(figsize=(10, 10))
-plt.scatter(x=aux["OOD"], y=aux["error"], label="Error")
-plt.xlabel("OOD Explanations")
-plt.ylabel("Error")
-plt.title("Error vs OOD Explanations")
-plt.show()
-# %%
-from scipy.stats import pearsonr
+    # Does the model decay?
+    print("Does the model decay?")
+    auc_te = np.round(roc_auc_score(y_te, detector.model.predict_proba(X_te)[:, 1]), 3)
+    auc_ood = np.round(
+        roc_auc_score(y_ood_te, detector.model.predict_proba(X_ood_te)[:, 1]), 3
+    )
+    print("AUC TE", auc_te)
+    print("AUC OOD", auc_ood)
 
-pearsonr(aux["error"], aux["OOD"])
-# %%
-import seaborn as sns
+    # Does the model G have good performance?
+    print("Does the model G have good performance?")
+    print("g: ", detector.get_auc_val())
 
-sns.kdeplot(aux["OOD"])
-# %%
+    # Analyze
+    aux = X_hold_test.copy()
+    aux["real"] = y_hold_test["real"].values
+    aux["pred"] = detector.model.predict(X_hold_test)
+    aux["pred_proba"] = detector.model.predict_proba(X_hold_test)[:, 1]
+    aux["ood"] = z_hold_test.values
+    aux["ood_pred"] = detector.predict(X_hold_test)
+    aux["ood_pred_proba"] = detector.predict_proba(X_hold_test)[:, 1]
+    print("Total flagged as OOD: ", aux[aux["ood_pred"] == 1].shape[0])
+
+    try:
+        decay = roc_auc_score(
+            aux[aux["ood_pred"] == 0].real, aux[aux["ood_pred"] == 0].pred_proba.values
+        ) - roc_auc_score(
+            aux[aux["ood_pred"] == 1].real, aux[aux["ood_pred"] == 1].pred_proba.values
+        )
+    except:
+        decay = 0
+    print(space, decay)
+
+    # %%
