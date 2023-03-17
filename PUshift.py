f0df4b50741247149322878929d3494a19f4da14
Carlos Mougan <carmougan@gmail.com>
2023-03-17 09:59:13 +0100

    pushift image

--- PUshift.py
+++ PUshift.py
@@ -3,12 +3,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+plt.style.use("seaborn-whitegrid")
+rcParams["axes.labelsize"] = 14
+rcParams["xtick.labelsize"] = 12
+rcParams["ytick.labelsize"] = 12
+rcParams["figure.figsize"] = 16, 8
+rcParams.update({"font.size": 22})
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from nobias import ExplanationShiftDetector
 from xgboost import XGBRegressor, XGBClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
+
 
 # %%
 
@@ -32,7 +40,6 @@ for r in [1, 8, 6]:
         df_tr.drop("y", axis=1), df_tr["y"], test_size=0.2, random_state=42
     )
     X_ood = df[df["Race"] == r].drop("y", axis=1)
-    y_ood = df[df["Race"] == r]["y"]
 
     detector = ExplanationShiftDetector(
         model=XGBClassifier(), gmodel=LogisticRegression()
@@ -43,24 +50,29 @@ for r in [1, 8, 6]:
 
     aucs_temp = []
     for i in params:
-
         X_ = X_ood.sample(frac=i, random_state=42, replace=False)
-        y_ = y_ood[X_.index]
         X_new = X_te.append(X_).drop(columns=["Race"])
-        y_new = y_te.append(y_)
 
         detector.fit(X_tr.drop(columns=["Race"]), y_tr, X_new)
-        aucs_temp.append(roc_auc_score(y_new, detector.predict(X_new)))
+        aucs_temp.append(detector.get_auc_val())
 
     aucs[r] = aucs_temp
 # %%
 
 # Plot
 plt.figure()
-plt.title(r)
 for r in [1, 8, 6]:
-    plt.plot(params, aucs[r], label=r)
+    if r == 1:
+        label = "White"
+    elif r == 8:
+        label = "Black"
+    else:
+        label = "Asian"
+    plt.plot(params, aucs[r], label=label)
 plt.xlabel("Fraction of OOD data")
 plt.ylabel("AUC of Explanation Shift Detector")
+plt.savefig("images/PUshift.png")
 plt.legend()
 
+
+# %%
